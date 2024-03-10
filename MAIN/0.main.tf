567b23eebeece5c6a90eb97bc97f9862c1d937d3
MSierski <92363171+MSierski@users.noreply.github.com>
2024-03-10 11:06:20 +0100

    Update 0.main.tf

--- MAIN/0.main.tf
+++ MAIN/0.main.tf
@@ -5,12 +5,11 @@ provider "azurerm" {
   features {}
 }
 
-#TODO
-#terraform {
-#  backend "azurerm" {
-#    storage_account_name = "<ACCOUNT NAME>" 
-#    container_name       = "<CONTAINER>" 
-#    key                  = "cloudeng.shared.terraform.tfstate"  
-#    access_key  = 
-#  }
-#}
+terraform {
+   backend "azurerm" {
+    resource_group_name  = "tfStateRG"
+    storage_account_name = "tfstatemeritoms"
+    container_name       = "tfstate1"
+    key                  = "tfstateAZ.tf"
+  }
+}
